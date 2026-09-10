54eec8c87b7883d2a617bac3504e81099b0c4aa8
ldaniel2-alt <ldaniel2@estudantes.unisced.edu.mz>
2026-09-10 11:11:16 +0200

    feat: add application entrypoint

---
+++ backend/src/main/java/mz/gov/mogovolas/expedientes/GestaoExpedientesApplication.java
@@ -0,0 +1,5 @@
+package mz.gov.mogovolas.expedientes;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class GestaoExpedientesApplication { public static void main(String[] args){ SpringApplication.run(GestaoExpedientesApplication.class,args); } }
